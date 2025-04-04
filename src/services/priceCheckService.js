05ae9255d2694e96789b906479007f44d07b4d27
Samson Ajax <ajax@Samsons-MacBook-Air.local>
2025-04-04 14:17:38 +0100

    first commit

--- src/services/priceCheckService.js
+++ src/services/priceCheckService.js
@@ -1,6 +1,12 @@
-// src/services/priceCheckService.js
 const products = require("../data/products.json");
+
+// Function to check all products for price drops
 const checkProductPrice = async () => {
-    return products.find(product => product.price <= product.desiredPrice) || null;
+    // Filter out products whose price is less than or equal to the desired price
+    const priceDrops = products.filter(product => product.price <= product.desiredPrice);
+    
+    // If price drops are found, return them, otherwise return null
+    return priceDrops.length > 0 ? priceDrops : null;
 };
+
 module.exports = { checkProductPrice };
